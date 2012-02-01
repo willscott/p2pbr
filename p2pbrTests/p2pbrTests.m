3bd5d3d26e75de84e495447eb747c748494b1015
Will Scott <willscott@gmail.com>
2012-01-31 21:13:54 -0800

    moved to p2pbr

---
+++ p2pbrTests/p2pbrTests.m
@@ -0,0 +1,32 @@
+//
+//  ChatClientTests.m
+//  ChatClientTests
+//
+//  Created by willscott@gmail.com on 1/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "p2pbrTests.h"
+
+@implementation p2pbrTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in p2pbrTests");
+}
+
+@end
