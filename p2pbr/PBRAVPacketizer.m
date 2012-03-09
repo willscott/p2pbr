f49b27cfff50b9c6f12a180f68c2bb7b9487501d
Adam Lerner <eletari@gmail.com>
2012-03-09 13:16:49 -0800

    Two AVAssetWriters going at once

--- p2pbr/PBRAVPacketizer.m
+++ p2pbr/PBRAVPacketizer.m
@@ -11,23 +11,38 @@
 
 @interface PBRAVPacketizer()
 
-@property (strong, nonatomic) AVAssetWriterInput* videoInput;
-@property (strong, nonatomic) AVAssetWriterInput* audioInput;
-@property (strong, nonatomic) AVAssetWriter* mediaOutput;
+
+// We have mediaOutput0 (with video/audioInputs0) and 1
+// One gets ready to receive data while the other one is receiving dataa
+// When we're finished with the currently working one, it is asynchronously told
+// to finishWriting and we start sending data to the other one.
+@property (strong, nonatomic) AVAssetWriterInput* videoInputActive;
+@property (strong, nonatomic) AVAssetWriterInput* audioInputActive;
+@property (strong, nonatomic) AVAssetWriter* mediaOutputActive;
+@property (strong, nonatomic) AVAssetWriterInput* videoInputNext;
+@property (strong, nonatomic) AVAssetWriterInput* audioInputNext;
+@property (strong, nonatomic) AVAssetWriter* mediaOutputNext;
+
 @property (strong, nonatomic) NSDate* segmentStart;
 
 -(NSURL*) getTemporaryFile;
 
 - (void) handleVideoSample:(CMSampleBufferRef)sampleBuffer;
 - (void) handleAudioSample:(CMSampleBufferRef)sampleBuffer;
-- (void) sendFileAndClean:(NSURL*)file;
+- (void) finishWritingFileAndSendAndClean:(AVAssetWriter*)writer;
+-(AVAssetWriter*) createNewAVAssetWriter;
 @end
 
 @implementation PBRAVPacketizer
 
-@synthesize videoInput = _videoInput;
-@synthesize audioInput = _audioInput;
-@synthesize mediaOutput = _mediaOutput;
+// mediaOutputs and their a/vInput
+@synthesize videoInputActive = _videoInputActive;
+@synthesize audioInputActive = _audioInputActive;
+@synthesize mediaOutputActive = _mediaOutputActive;
+@synthesize videoInputNext = _videoInputNext;
+@synthesize audioInputNext = _audioInputNext;
+@synthesize mediaOutputNext = _mediaOutputNext;
+
 @synthesize segmentStart = _segmentStart;
 
 @synthesize active = _active;
@@ -46,8 +61,10 @@
   
 
   if (!audio || !video) {
-    self.videoInput = nil;
-    self.audioInput = nil;
+    self.videoInputActive = nil;
+    self.audioInputActive = nil;
+    self.videoInputNext = nil;
+    self.audioInputNext = nil;
     return;
   }
 
@@ -59,9 +76,12 @@
                                         [NSNumber numberWithInt:640], AVVideoWidthKey,
                                         [NSNumber numberWithInt:480], AVVideoHeightKey,
                                         nil];
-  AVAssetWriterInput* videoInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo 
+  AVAssetWriterInput* videoInputActive = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo 
                                                                       outputSettings:videoOutputSettings];
-  videoInput.expectsMediaDataInRealTime = YES;
+  AVAssetWriterInput* videoInputNext = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeVideo 
+                                                                            outputSettings:videoOutputSettings];
+  videoInputActive.expectsMediaDataInRealTime = YES;
+  videoInputNext.expectsMediaDataInRealTime = YES;
 
   AudioChannelLayout cl;
   bzero(&cl, sizeof(cl));
@@ -74,12 +94,39 @@
                                        [NSNumber numberWithInt: 64000],                AVEncoderBitRateKey,
                                        [NSData dataWithBytes: &cl length:sizeof(cl)],  AVChannelLayoutKey,
                                        nil];
-  AVAssetWriterInput* audioInput = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio 
+  AVAssetWriterInput* audioInputActive = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio 
                                                                       outputSettings:audioOutputSettings];
-  audioInput.expectsMediaDataInRealTime = YES;
+  AVAssetWriterInput* audioInputNext = [AVAssetWriterInput assetWriterInputWithMediaType:AVMediaTypeAudio 
+                                                                      outputSettings:audioOutputSettings];
+  audioInputActive.expectsMediaDataInRealTime = YES;
+  audioInputNext.expectsMediaDataInRealTime = YES;
   
-  self.videoInput = videoInput;
-  self.audioInput = audioInput;
+  self.videoInputActive = videoInputActive;
+  self.audioInputActive = audioInputActive;
+  self.videoInputNext = videoInputNext;
+  self.audioInputNext = audioInputNext;
+  
+  self.mediaOutputActive = [self createNewAVAssetWriter];
+  self.mediaOutputNext = [self createNewAVAssetWriter];
+  
+  [self.mediaOutputActive addInput:self.videoInputActive];
+  [self.mediaOutputActive addInput:self.audioInputActive];
+  [self.mediaOutputNext addInput:self.videoInputNext];
+  [self.mediaOutputNext addInput:self.audioInputNext];
+  
+  [self.mediaOutputActive startWriting];
+  [self.mediaOutputNext startWriting];
+  
+  /** This determines that an empty data is 207 bytes
+  AVAssetWriter* test = [self createNewAVAssetWriter];
+  [test startWriting];
+  [test startSessionAtSourceTime:CMTimeMakeWithSeconds(0, 1)];
+  [test finishWriting];
+  NSData* testData = [NSData dataWithContentsOfURL:test.outputURL];
+  NSLog(@"%d", [testData length]);
+   */
+
+  
 }
 
 -(void) setActive:(BOOL)active
@@ -110,82 +157,146 @@
 {
   // Finish Active segment.
   if (self.segmentStart && [[NSDate date] timeIntervalSinceDate:self.segmentStart] > SEGMENT_LENGTH) {
-    if (![self.mediaOutput finishWriting]) {
-      NSLog(@"Unable to finish writing segment output.");
+    AVAssetWriter* finished = self.mediaOutputActive;
+    if (self.mediaOutputNext == nil) {
+      NSLog(@"Next media output not ready by the time previous one finished.");
       return;
     }
-    self.segmentStart = nil;
+    if (self.mediaOutputNext.status != AVAssetWriterStatusWriting) {
+      NSLog(@"Warning: switching to next writer with status %d", self.mediaOutputNext.status);
+      return;
+    }
+    NSLog(@"finished with writer %p in status %d, switching to next writer %p with status %d", finished, finished.status, self.mediaOutputNext, self.mediaOutputNext.status);
+    self.mediaOutputActive = self.mediaOutputNext;
+    self.videoInputActive = self.videoInputNext;
+    self.audioInputActive = self.audioInputNext;
+    self.mediaOutputNext = nil;   // by the time we get around to the next segment, this should have been
+                                  // created anew asynchronously by the following call
+    
+    // In another thread, finish writing the newly finished segment to file and then to network
+    [self performSelectorInBackground:@selector(finishWritingFileAndSendAndClean:) withObject:finished];
 
-    [self performSelectorInBackground:@selector(sendFileAndClean:) withObject:self.mediaOutput.outputURL];
-    self.mediaOutput = nil;
+    self.segmentStart = nil;
   }
 
   if (self.active) {
+    // Start first segment of new series of recordings.
+    if (!self.segmentStart) {
+      self.segmentStart = [NSDate date];
+      if (self.mediaOutputActive.status != AVAssetWriterStatusWriting) {
+        [self.mediaOutputActive startWriting];
+        //NSLog(@"called startWriting");
+
+//        NSLog(@"Warning: active writer status is %d", self.mediaOutputActive.status);
+//        return;
+      }
+      CMTime tstamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
+      [self.mediaOutputActive startSessionAtSourceTime:tstamp];
+      //NSLog(@"started session with timestamp %f on output %@", CMTimeGetSeconds(tstamp), self.mediaOutputActive);
+    }
     if (!CMSampleBufferDataIsReady(sampleBuffer)) {
       NSLog(@"Skipping premature buffer.");
       return;
     }
-    
-    // Start new segment.
-    if (!self.mediaOutput) {
-        self.segmentStart = [NSDate date];
-        NSError *err = nil;
-        self.mediaOutput = [[AVAssetWriter alloc] initWithURL:[self getTemporaryFile]
-                                                     fileType:AVFileTypeMPEG4 
-                                                        error:&err];
-        [self.mediaOutput addInput:self.videoInput];
-        [self.mediaOutput addInput:self.audioInput];
-        
-        //Metadata
-        [self.mediaOutput setCommonMetadata:[NSDictionary dictionaryWithObjectsAndKeys:
-                                             @"p2pbr", AVMetadataCommonKeyTitle,
-                                             @"p2pbr", AVMetadataCommonKeyPublisher,
-                                             @"iOS p2pbr", AVMetadataCommonKeySoftware,
-                                             nil]];
-      [self.mediaOutput startWriting];
-      CMTime tstamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer);
-      [self.mediaOutput startSessionAtSourceTime:tstamp];
-    }
-
-    if (self.mediaOutput.status != AVAssetWriterStatusWriting) {
-      NSLog(@"Warning: writer status is %d", self.mediaOutput.status);
-      return;
-    }
-
+     
+    //video
     if ([captureOutput isKindOfClass:[AVCaptureVideoDataOutput class]]) {
       [self handleVideoSample:sampleBuffer];
-    } else if([captureOutput isKindOfClass:[AVCaptureAudioDataOutput class]]) {
+    } 
+    //audio
+    else if([captureOutput isKindOfClass:[AVCaptureAudioDataOutput class]]) {
       [self handleAudioSample:sampleBuffer];      
     }
+
+  
   }
 }
 
 - (void) handleVideoSample:(CMSampleBufferRef)sampleBuffer
 {
-  if(![self.videoInput appendSampleBuffer:sampleBuffer]) {
+  if(![self.videoInputActive appendSampleBuffer:sampleBuffer]) {
     NSLog(@"Unable to add video sample.");
   }
 }
 
 - (void) handleAudioSample:(CMSampleBufferRef)sampleBuffer
 {
-  if(![self.audioInput appendSampleBuffer:sampleBuffer]) {
+  if(![self.audioInputActive appendSampleBuffer:sampleBuffer]) {
     NSLog(@"Unable to add audio sample.");
   }
 }
 
-- (void) sendFileAndClean:(NSURL*)file
-{
-  NSData* recordedData = [NSData dataWithContentsOfURL:file];
 
+- (void) finishWritingFileAndSendAndClean:(AVAssetWriter*)writer
+{
+  // **********
+  // old writer: finish writing
+  // send out old data
+  // clean up old data's file
+  // 
+  // new writer: create
+  // new writer: configure
+  // new writer: add old writer's inputs
+  // new writer: set as next
+  // self.inputNexts: set as new writer's inputs
+  // new writer: start writing
+  // **********
+  
+  NSLog(@"About to call finishWriting on writer.  Status: %d, Error: %@", writer.status, writer.error);
+  if (![writer finishWriting]) {
+    NSLog(@"Unable to finish writing segment output. Status: %d, Error: %@", writer.status, writer.error);
+    return;
+  }
+  
+  NSData* recordedData = [NSData dataWithContentsOfURL:writer.outputURL];
+  NSLog(@"Recorded data is %d bytes", [recordedData length]);
   [self.socket sendData:recordedData andThen:^(BOOL success) {
     NSLog(@"Cleaned up after sending segment.");
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    if ([fileManager fileExistsAtPath:[file path]]) {
-      [fileManager removeItemAtURL:file error: nil];
+    if ([fileManager fileExistsAtPath:[writer.outputURL path]]) {
+      [fileManager removeItemAtURL:writer.outputURL error: nil];
     }
   }];
 
+  AVAssetWriter* newWriter = [self createNewAVAssetWriter];
+ 
+  // Add old writer's inputs on new writer, and
+  // set old writer's inputs as next inputs
+  for (AVAssetWriterInput* input in writer.inputs) {
+    [newWriter addInput:input];
+    if ([input.mediaType isEqualToString:AVMediaTypeVideo]) {
+      self.videoInputNext = input;
+    }
+    else if ([input.mediaType isEqualToString:AVMediaTypeAudio]) {
+      self.audioInputNext = input;
+    }
+    else {
+      NSLog(@"Old writer had inputs not of type Audio or Video");
+    }
+  }
+  [newWriter startWriting];
+  
+  self.mediaOutputNext = newWriter;
+  
+  if (self.mediaOutputNext.status != AVAssetWriterStatusWriting) {
+    NSLog(@"Warning: next writer status is %d", self.mediaOutputNext.status);
+    return;
+  }
+  NSLog(@"Done setting up next writer, it is in state writing");
+}
+
+- (AVAssetWriter*) createNewAVAssetWriter {
+  NSError *err = nil;
+  AVAssetWriter* newWriter = [[AVAssetWriter alloc] initWithURL:[self getTemporaryFile]
+                                                    fileType:AVFileTypeMPEG4                    
+                                                    error:&err];
+  //Metadata
+  [newWriter setCommonMetadata:[NSDictionary dictionaryWithObjectsAndKeys:
+                                @"p2pbr", AVMetadataCommonKeyTitle,
+                                @"p2pbr", AVMetadataCommonKeyPublisher,
+                                @"iOS p2pbr", AVMetadataCommonKeySoftware,
+                                nil]];
+  return newWriter;
 }
 
 @end
